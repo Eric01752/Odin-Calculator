a49238b2ff38ba0d7245bc38181187a416fc30bc
eric <eric.schmidt1350@gmail.com>
2021-12-22 17:06:49 -0500

    Operate function

--- script.js
+++ script.js
@@ -14,3 +14,17 @@ function divide(a, b) {
     return (a / b);
 }
 
+function operate(operation, a, b) {
+    if(operation === '+') {
+        return add(a, b);
+    }
+    else if(operation === '-') {
+        return subtract(a, b);
+    }
+    else if(operation === '*') {
+        return multiply(a, b);
+    }
+    else if(operation === '/') {
+        return divide(a, b);
+    }
+}
