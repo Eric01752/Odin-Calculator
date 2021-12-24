0744a94aa9f505a6274e046d7f360edd1455b3d3
eric <eric.schmidt1350@gmail.com>
2021-12-23 22:19:18 -0500

    Operation button functionality

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
+let displayInputLogic = "0";
+let displayOperationLogic = "";
+let operation;
+let num1;
+
+const displayInput = document.querySelector('.display-input');
+const displayOperation = document.querySelector('.display-operation');
+
+const btnNums = document.querySelectorAll('.btn-update');
+const btnOperations = document.querySelectorAll('.btn-operation');
+
+const btnClear = document.querySelector('#btn-clear');
+const btnEval = document.querySelector('#btn-eval');
+
+btnNums.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        if(displayInput.textContent === "0") {
+            displayInput.textContent = e.target.textContent;
+            displayInputLogic = e.target.textContent;
+        }
+        else {
+            displayInput.textContent += e.target.textContent;
+            displayInputLogic += e.target.textContent;
+        }
+    });
+});
+
+btnOperations.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        if(displayOperationLogic === "") {
+            displayOperation.textContent += (displayInputLogic + e.target.textContent);
+            displayOperationLogic += (displayInputLogic + e.target.textContent);
+            displayInputLogic = "0";
+            displayInput.textContent = displayInputLogic;
+            operation = e.target.textContent;
+            num1 = parseInt(displayOperationLogic.split(operation));
+        }
+        else {
+            let calculation = operate(operation, num1, parseInt(displayInputLogic.split(operation))).toString();
+            operation = e.target.textContent;
+            displayOperationLogic = (calculation + operation);
+            displayOperation.textContent = displayOperationLogic;
+            displayInputLogic = "0";
+            displayInput.textContent = displayInputLogic;
+            num1 = parseInt(displayOperationLogic.split(operation));
+        }
+    });
+});
+
 function add(a, b) {
     return (a + b);
 }
